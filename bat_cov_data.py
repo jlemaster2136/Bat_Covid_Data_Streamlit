1185db0dbe7b905ea5f781e0924c32d18b3d59af
jlemaster2136 <140013029+jlemaster2136@users.noreply.github.com>
2024-10-28 17:09:25 +0000

    updated_with_comparison

--- bat_cov_data.py
+++ bat_cov_data.py
@@ -7,24 +7,44 @@ import plotly.graph_objects as go
 
 data = pd.read_csv('/workspaces/codespaces-blank/exported-table.csv')
 
-data[['Bat Genus', 'Bat Species']] = data.pop('Bat species').str.split(' ', n=1, expand=True)
+data[['Bat genus', 'Bat Species']] = data.pop('Bat species').str.split(' ', n=1, expand=True)
 data['Year'] = data.pop('Study').str[-4:]
 data = data.drop(columns=['🔗'])
 data = data[(data['Study type'] == 'cross-sectional')]
 data = data.astype('int', errors='ignore')
-print(data)
+
+
+
+genus_list = ['betacoronavirus', 'alphacoronavirus']
+color_scheme = ['lightgreen', 'skyblue']
 
 
 
 def by_virus_genus_and_filter(dataset, genus, filter):
-    genus = genus.lower()
-    dataset = dataset[(dataset['Virus genus']==genus)]
-    dataset = dataset.groupby(filter).sum()
+    if genus == 'Compare them!':
+        if filter in ['Year', 'Sample tissue']:
+            dataset = dataset.groupby([filter, 'Virus genus']).sum()
+        else:
+            dataset = dataset[(dataset['Virus genus'].isin(genus_list))]
+            total_genera = dataset['Virus genus'].nunique()
+            genera_per_country = dataset.groupby(filter)['Virus genus'].nunique()
+            filter_is_one_to_all = genera_per_country == total_genera
+            dataset['keep'] = dataset[filter].map(filter_is_one_to_all)
+            dataset = dataset[(dataset['keep'] == True)]
+            dataset = dataset.drop('keep', axis=1)
+            dataset = dataset.groupby([filter, 'Virus genus']).sum()
+    elif genus == 'All':
+        dataset = dataset.groupby(filter).sum()
+    else:
+        dataset = dataset[(dataset['Virus genus']==genus.lower())]
+        dataset = dataset.groupby(filter).sum()
+
     dataset['proportion'] = dataset['+'] / dataset['#']
     dataset['prop_error'] = np.sqrt(dataset['proportion'] * (1 - dataset['proportion']) / dataset['#'])
     dataset = dataset.sort_values('proportion', ascending=False)
     dataset = dataset[(dataset['#']>10)]
     return dataset[['+', '#', 'proportion', 'prop_error']].reset_index()
+
 
 st.title('Coronavirus Prevalence in Bats')
 
@@ -40,40 +60,86 @@ with tab1:
 
     by = st.selectbox(
         "Prevalence by:",
-        ('Year', 'Country', 'Bat Genus', 'Sample Type'),
+        ('Year', 'Country', 'Bat genus', 'Sample tissue'),
         index=None,
         placeholder="Select filter",
     )
 
 
     if st.button('Generate Chart'):
-        data = by_virus_genus_and_filter(data, option, by)[[by, 'proportion', 'prop_error', '#']]
-        x = data[by]
-        y = data['proportion']
-        y_error = data['prop_error']
-        
-        fig = go.Figure(
-                data=go.Bar(
-                    x=x,
-                    y=y,
-                    error_y=dict(type='data', array=y_error, visible=True),
-                    marker_color='skyblue',
-                    name='Bar with Error',
-                    text=data['#'],  # Attach `#` column data for hover
-                    hovertemplate=(
-                        "Category: %{x}<br>"            # x-axis label
-                        "Proportion: %{y:.2f} ± %{error_y.array:.2f}<br>"  # y-value and error
-                        "n: %{text}<br>"  # `#` column data
-                        "<extra></extra>"                # Hides extra trace info
-                    ),
-                    textposition='none',
+        if option == 'Compare them!':
+            data = by_virus_genus_and_filter(data, option, by)[['Virus genus', by, 'proportion', 'prop_error', '#']]
+            fig = go.Figure()
+            for i in range(len(genus_list)):
+                data_genus = data[(data['Virus genus']==genus_list[i])]
+                print(genus_list[i])
+                x = data_genus[by]
+                y = data_genus['proportion']
+                y_error = data_genus['prop_error']
+                name = genus_list[i]
+                fig.add_trace(
+                    go.Bar(
+                        x=x,
+                        y=y,
+                        error_y=dict(type='data', array=y_error, visible=True),
+                        marker_color=color_scheme[i],
+                        name=genus_list[i],
+                        text=data_genus['#'],  # Attach `#` column data for hover
+                        hovertemplate=(
+                            "Category: %{x}<br>"            # x-axis label
+                            "Proportion: %{y:.2f} ± %{error_y.array:.2f}<br>"  # y-value and error
+                            "n: %{text}<br>"  # `#` column data
+                            "<extra></extra>"                # Hides extra trace info
+                        ),
+                        textposition='none',
+                    )
                 )
-            )
-        # Add titles
-        fig.update_layout(title="Bar Chart with Error Bars", xaxis_title="Categories", yaxis_title="Values")
+                        # Add titles
+            fig.update_layout(
+                title={
+                    'text': f'Positive Coronavirus Samples by {by}',
+                    'x': 0.5,  # Center the title (0 is left, 0.5 is center, 1 is right)
+                    'xanchor': 'center'}, 
+                xaxis_title=by, 
+                yaxis_title="Proportion of positive samples", 
+                )
 
-        # Display in Streamlit
-        st.plotly_chart(fig)
+            # Display in Streamlit
+            st.plotly_chart(fig)
+        else:
+            data = by_virus_genus_and_filter(data, option, by)[[by, 'proportion', 'prop_error', '#']]
+            x = data[by]
+            y = data['proportion']
+            y_error = data['prop_error']
+            fig = go.Figure(
+                    data=go.Bar(
+                        x=x,
+                        y=y,
+                        error_y=dict(type='data', array=y_error, visible=True),
+                        marker_color='skyblue',
+                        name='Bar with Error',
+                        text=data['#'],  # Attach `#` column data for hover
+                        hovertemplate=(
+                            "Category: %{x}<br>"            # x-axis label
+                            "Proportion: %{y:.2f} ± %{error_y.array:.2f}<br>"  # y-value and error
+                            "n: %{text}<br>"  # `#` column data
+                            "<extra></extra>"                # Hides extra trace info
+                        ),
+                        textposition='none',
+                    )
+                )
+            # Add titles
+            fig.update_layout(
+                title={
+                    'text': f'Positive Coronavirus Samples by {by}',
+                    'x': 0.5,  # Center the title (0 is left, 0.5 is center, 1 is right)
+                    'xanchor': 'center'}, 
+                xaxis_title=by, 
+                yaxis_title="Proportion of positive samples", 
+                )
+
+            # Display in Streamlit
+            st.plotly_chart(fig)
             
 url = 'Cohen, L.E., Fagre, A.C., Chen, B. et al. Coronavirus sampling and surveillance in bats from 1996–2019: a systematic review and meta-analysis. Nat Microbiol 8, 1176–1186 (2023). https://doi.org/10.1038/s41564-023-01375-1'
 
