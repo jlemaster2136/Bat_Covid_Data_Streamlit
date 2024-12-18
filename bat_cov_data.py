7e2cbd5aeca9bd44d2b5bad9fca0cc716ca6c6bb
jlemaster2136 <140013029+jlemaster2136@users.noreply.github.com>
2024-12-18 13:03:07 -0500

    Update bat_cov_data.py

--- bat_cov_data.py
+++ bat_cov_data.py
@@ -63,7 +63,7 @@ def by_virus_genus_and_filter(dataset, genus, filter):
     return dataset[['+', '#', 'proportion', 'prop_error']].reset_index()
 
 
-st.title('Coronavirus epidemiology explorer')
+st.title('Coronavirus ecology explorer')
 
 #Create Streamlit tabs.
 tab1, tab2, tab3 = st.tabs(["Data Visualization", "About", "References"])
